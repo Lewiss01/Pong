a1503373961e8a7e264beb8ab9686485c1dfb8e1
Lewiss01 <lewis.mtz@gmail.com>
2023-05-10 20:28:48 -0300

    Implemented movement player and points

--- Pong/Assets/Scripts/BallBehavior.cs
+++ Pong/Assets/Scripts/BallBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BallBehavior : MonoBehaviour
 {
-    [SerializeField] float ballSpeed = 7f;
+    [SerializeField] float ballSpeed = 3f;
     private float positionStartX;
     private float positionStartY;
 
@@ -15,13 +15,26 @@ public class BallBehavior : MonoBehaviour
         RandomStart();
     }
 
-    private void RandomStart()
+    public void RandomStart()
     {
         positionStartX = Random.Range(0, 2) == 0 ? 1 : -1;
         positionStartY = Random.Range(0, 2) == 0 ? 1 : -1;
         rb2D.velocity = new Vector2(positionStartX, positionStartY)* ballSpeed;
-
-
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ArcheryP1"))
+        {
+            GameController.Connector.p2Scored();
+            GameController.Connector.ReStart();
+            RandomStart();
+        }
+        else
+        {
+            GameController.Connector.p1Scored();
+            GameController.Connector.ReStart();
+            RandomStart();
+        }
     }
 
 
---
+++ Pong/Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditor.Build.Content;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textScoreP1;
+    private int scoreP1;
+    
+    [SerializeField] private TMP_Text textScoreP2;
+    private int scoreP2;
+
+    [SerializeField] private Transform player1Transform;
+    [SerializeField] private Transform player2Transform;
+    [SerializeField] private Transform ballTransform;
+
+
+
+    // al usar un silgletone para facilitar
+    // la comunicacion con los otros cripts del juego
+
+
+    private static GameController connector;
+
+    public static GameController Connector
+    {
+        get
+        {
+            if (connector == null)
+            {
+                connector = FindObjectOfType<GameController>();
+            }
+            return connector;
+        }      
+    }
+    public void p1Scored()
+    {
+        scoreP1++;
+        textScoreP1.text = scoreP1.ToString();
+    }
+    public void p2Scored()
+    {
+        scoreP2++;
+        textScoreP2.text = scoreP2.ToString();
+    }
+
+    public void ReStart()
+    {
+        player1Transform.position = new Vector2(player1Transform.position.x, 0);
+        player2Transform.position = new Vector2(player2Transform.position.x, 0);
+        ballTransform.position = new Vector2(0, 0);
+    } 
+
+}
+
